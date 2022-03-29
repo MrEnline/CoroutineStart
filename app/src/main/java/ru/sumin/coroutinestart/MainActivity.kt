7a1eb924e6266a5384894841a3aab9f6417a5f8e
Kuznetsov Sergey <mr.enline@gmail.com>
2022-03-29 23:01:37 +0300

    Реализация handler

--- app/src/main/java/ru/sumin/coroutinestart/MainActivity.kt
+++ app/src/main/java/ru/sumin/coroutinestart/MainActivity.kt
@@ -1,6 +1,7 @@
 package ru.sumin.coroutinestart
 
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
@@ -12,6 +13,8 @@ class MainActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
+    private val handler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,19 +40,25 @@ class MainActivity : AppCompatActivity() {
     private fun loadCity(callback: (String) -> Unit) {
         thread{
             Thread.sleep(5000)
-            callback("Moscow")
+            handler.post {
+                callback("Moscow")
+            }
         }
     }
 
     private fun loadTemperature(city: String, callback: (Int) -> Unit) {
         thread {
-            Toast.makeText(
-                this,
-                getString(R.string.loading_temperature_toast, city),
-                Toast.LENGTH_SHORT
-            ).show()
+            handler.post {
+                Toast.makeText(
+                    this,
+                    getString(R.string.loading_temperature_toast, city),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
             Thread.sleep(5000)
-            callback(17)
+            handler.post {
+                callback(17)
+            }
         }
     }
 }
